da460efee5139dd83d1a1038b9ddc3f0f2997e30
Nicolò Battaglia <gaara0633@gmail.com>
2020-06-03 15:52:13 +0200

    Fix printMe

--- index.php
+++ index.php
@@ -6,7 +6,13 @@
         public $colore;
 
         function __construct($quantita){
-            $this -> quantita = $quantita;
+            $this->quantita = $quantita;
+        }
+
+        public function printMe() {
+            echo "Quantità:" . $this->quantita . " "
+                 . "Peso:" . $this->peso . " "
+                 . "Colore:" . $this->colore . "<br>";
         }
     }
     
@@ -14,18 +20,19 @@
     $carote = new Prodotto(10);
     $carote->peso = "10kg";
     $carote->colore = "Arancio";
-    echo "Quantià Carote:" . " " . $carote->quantita . " " . "Peso Carote:" . " " . $carote->peso . " " . "Colore Carote:" . " " . $carote->colore;
-    echo "<br>";
 
+    
     $patate = new Prodotto(15);
     $patate->peso = "15kg";
     $patate->colore = "Giallo";
-    echo "Quantià Patate:" . " " . $patate->quantita . " " . "Peso Patate:" . " " . $patate->peso . " " . "Colore Patate:" . " " . $patate->colore;
-    echo "<br>"; 
-    
+
     
     $pomodori = new Prodotto(20);
     $pomodori->peso = "20kg";
     $pomodori->colore = "Rosso";
-    echo "Quantià Pomodori:" . " " . $pomodori->quantita . " " . "Peso Pomodori:" . " " . $pomodori->peso . " " . "Colore Pomodori:" . " " . $pomodori->colore;
+
+    $carote->printMe();
+    $patate->printMe();
+    $pomodori->printMe();
+
 ?>
